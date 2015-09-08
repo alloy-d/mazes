59b588421dfeec43a8e3c763190d9c2655966026
Adam Lloyd <adam@alloy-d.net>
2015-09-08 19:31:52 -0400

    Now with linking!

--- src/mazes/core.clj
+++ src/mazes/core.clj
@@ -10,6 +10,28 @@
 (defn make-grid [rows cols]
   (vec (map (fn [_] (make-row cols)) (range rows))))
 
+(defn col [loc] (last loc))
+(defn row [loc] (first loc))
+
+;; Returns the wall that connects loc1 to loc2, from loc1's perspective.
+(defn connection [loc1 loc2]
+  (let [col-offset (- (col loc2) (col loc1))
+        row-offset (- (row loc2) (row loc1))]
+    (cond (= col-offset 0) (cond (= row-offset 1) :bottom
+                                 (= row-offset -1) :top
+                                 :else nil)
+          (= row-offset 0) (cond (= col-offset 1) :right
+                                 (= col-offset -1) :left
+                                 :else nil)
+          :else nil)))
+
+(defn link [grid loc1 loc2]
+  (let [cell1 (get-in grid loc1)
+        cell2 (get-in grid loc2)]
+    (assoc-in (assoc-in grid loc1
+                        (assoc cell1 (connection loc1 loc2) false))
+              loc2 (assoc cell2 (connection loc2 loc1) false))))
+
 (defn string-for-part [has-wall? wall-glyph passage-glyph closer]
   (let
       [draw-cell (fn [cell]
