0410aea28f8a1e48f33a19fcacce5d6e7c51759a
Adam Lloyd <adam@alloy-d.net>
2015-09-08 18:32:38 -0400

    Add some incomplete and non-functional REPL-friendliness boilerplate.
    Inspired by http://thinkrelevance.com/blog/2013/06/04/clojure-workflow-reloaded

---
+++ dev/user.clj
@@ -0,0 +1,4 @@
+(ns user
+  (:require [clojure.pprint :refer (pprint)]
+            [clojure.repl :refer :all]
+            [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot mazes.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                                  [org.clojure/java.classpath "0.2.0"]]}})
