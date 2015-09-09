806600ae8691c68b752d48accc79ca2e1521c388
Adam Lloyd <adam@alloy-d.net>
2015-09-08 21:37:48 -0400

    Move the ASCII representation code into its own namespace.

--- src/mazes/core.clj
+++ src/mazes/core.clj
@@ -32,47 +32,7 @@
                         (assoc cell1 (connection loc1 loc2) false))
               loc2 (assoc cell2 (connection loc2 loc1) false))))
 
-(defn string-for-part [has-wall? wall-glyph passage-glyph closer]
-  (let
-      [draw-cell (fn [cell]
-                   (if (has-wall? cell)
-                     wall-glyph
-                     passage-glyph))]
-    (fn [row]
-      (str/join (flatten [(map draw-cell row) (closer (last row))])))))
 
-(def string-for-top (string-for-part
-                     :top
-                     "+---"
-                     "+   "
-                     (fn [_] "+")))
-
-(def string-for-middle (string-for-part
-                        :left
-                        "|   "
-                        "    "
-                        (fn [cell]
-                          (if (:right cell)
-                            "|"
-                            " "))))
-
-(def string-for-bottom (string-for-part
-                        :bottom
-                        "+---"
-                        "+   "
-                        (fn [_] "+")))
-
-(defn string-for-row [row]
-  (str/join "\n" [(string-for-top row)
-                  (string-for-middle row)]))
-
-(defn string-for-grid [grid]
-  (str/join "\n"
-            (flatten [(map string-for-row grid)
-                      (string-for-bottom (last grid))])))
-
-(defn print-grid [grid]
-  (println (string-for-grid grid)))
 
 (defn -main
   "I don't do a whole lot ... yet."
