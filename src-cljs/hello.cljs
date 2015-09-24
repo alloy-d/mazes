1b995aa9f221a9626bee2b5a7ac1e93123c76df4
Adam Lloyd <adam@alloy-d.net>
2015-09-23 23:09:32 -0400

    Futz with the appearance: shrink and vertically center.

--- src-cljs/hello.cljs
+++ src-cljs/hello.cljs
@@ -3,13 +3,13 @@
    [mazes.core :refer (make-grid)]
   ;[mazes.repr.ascii :refer (string-for-grid)]
    [mazes.repr.html-table :as table]
-   [mazes.alg.sidewinder :as alg]
+   [mazes.alg.binary-tree :as alg]
    [mazes.analysis.dijkstra :as analysis]
    [dommy.core :as dommy :refer-macros [sel sel1]]
    [clojure.string :as str]))
 
-(def height 46)
-(def width 26)
+(def height 31)
+(def width 52)
 (def base-grid (make-grid height width))
 
 ;; (defn draw-grid []
@@ -17,8 +17,15 @@
 ;;     (dommy/set-text! container (string-for-grid (alg/on base-grid)))))
 (defn draw-grid []
   (let [container (sel1 "#maze")]
-    (dommy/set-html! container
-                     (table/represent (analysis/compute-distances (alg/on base-grid) [(rand-int height) (rand-int width)])))))
+    (do
+      (dommy/set-html! container
+                       (table/represent (analysis/compute-distances (alg/on base-grid) [(rand-int height) (rand-int width)])))
+      (dommy/set-attr! container
+                       :style
+                       (str/join "" ["height: calc((1.5em + 0.15em) * "
+                                     height
+                                     " + 0.15em"
+                                     ");"])))))
 
 (js/setInterval draw-grid 5000)
 (draw-grid)
