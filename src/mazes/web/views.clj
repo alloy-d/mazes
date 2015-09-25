cfd20f63caf74300ce9374b84c9195f15fb7c88e
Adam Lloyd <adam@alloy-d.net>
2015-09-24 22:35:59 -0400

    Add a ridiculous cache-buster for EO1

--- src/mazes/web/views.clj
+++ src/mazes/web/views.clj
@@ -1,14 +1,18 @@
 (ns mazes.web.views
   (:require
+   [clojure.string :as str]
    [hiccup
     [page :refer [html5]]
     [page :refer [include-js include-css]]]))
+
+(defn- cache-bust [asset]
+  (str/join [asset "?" (.getTime (new java.util.Date))]))
 
 (defn index-page []
   (html5
    [:head
     [:title "Mazes"]
-    (include-css "css/main.css")]
+    (include-css (cache-bust "css/main.css?"))]
    [:body
     [:div {:id "maze" :class "maze"} "Generating your maze..."]
-    (include-js "js/out/main.js")]))
+    (include-js (cache-bust "js/out/main.js"))]))
