0593fa163caa868d7041abf87ca961aa9d3c171a
Adam Lloyd <adam@alloy-d.net>
2015-10-10 16:26:52 -0400

    Begin reimplementing a maze display with om.

--- src-cljs/mazes/web/core.cljs
+++ src-cljs/mazes/web/core.cljs
@@ -1,35 +1,64 @@
 (ns mazes.web.core
   (:require
    [mazes.core :refer (make-grid)]
-  ;[mazes.repr.ascii :refer (string-for-grid)]
+   [mazes.repr.ascii :refer (string-for-grid)]
    [mazes.repr.html-table :as table]
-   [mazes.alg.wilsons :as alg]
+   [mazes.alg.sidewinder :as alg]
    [mazes.analysis.dijkstra :as analysis]
+   [om.core :as om]
+   [om.dom :as dom]
+   [sablono.core :as html :refer-macros [html]]
    [dommy.core :as dommy :refer-macros [sel sel1]]
    [clojure.string :as str]))
 
-(def cell-size "2.5em")
-(def border-size "0.6em")
-(def height 53)
-(def width 30)
-(def base-grid (make-grid height width))
+(defn dimensions [node]
+  (let [bounds (.getBoundingClientRect node)]
+    {:height (.-height bounds)
+     :width (.-width bounds)}))
 
-;; (defn draw-grid []
-;;   (let [container (sel1 "#ascii-maze")]
-;;     (dommy/set-text! container (string-for-grid (alg/on base-grid)))))
-(defn draw-grid []
-  (let [container (sel1 "#maze")]
-    (do
-      (dommy/set-html! container
-                       (table/represent (analysis/compute-distances (alg/on base-grid) [(rand-int height) (rand-int width)])))
-      (dommy/set-attr! container
-                       :style
-                       (str/join "" ["height: calc("
-                                     "(" border-size " + " cell-size ")"
-                                     " * "
-                                     height
-                                     " + " border-size
-                                     ");"])))))
+(def get-maze
+  (memoize
+   (fn [height width gen-maze]
+     (gen-maze (make-grid height width)))))
 
-(js/setInterval draw-grid 15000)
-(draw-grid)
+(defn dumb-maze [data owner]
+  (reify
+    om/IDidMount
+    (did-mount [this]
+      (let [node (om/get-node owner)
+            bounds (.getBoundingClientRect node)
+            height (.-height bounds)
+            width (.-width bounds)]
+        (doto js/console
+          (.group "container size")
+          (.log "height" height)
+          (.log "width" width)
+          (.groupEnd "container size"))))
+
+    om/IRender
+    (render [this]
+      (dom/div
+       #js {:dangerouslySetInnerHTML
+            #js {:__html (table/represent (:grid data))}}))))
+
+(defn maze-world [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:height nil :width nil})
+
+    om/IDidMount
+    (did-mount [this]
+      (let [node (om/get-node owner)]
+        (do
+          (om/set-state! owner (dimensions node))
+          (dommy/listen! js/window :resize
+                         #(om/set-state! owner (dimensions node))))))
+
+    om/IRenderState
+    (render-state [this {:keys [:height :width]}]
+      (let [maze (get-maze (int (/ height 50)) (int (/ width 50)) alg/on)]
+        (om/build dumb-maze {:grid maze})))))
+
+(om/root maze-world {}
+         {:target (sel1 "#maze")})
